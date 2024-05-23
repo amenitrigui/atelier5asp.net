bd1eea6d3693803fb0aeea5e60d5081f2e58cfce
ameni <amenitriguii@gmailcom>
2024-05-23 19:34:08 +0100

    first commit

---
+++ Model/Repositories/IDepartmentRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace Atelier5.Model.Repositories
+{
+    public interface IDepartmentRepository
+    {
+        IEnumerable<Department> GetDepartments();
+        Department GetDepartment(int departmentId);
+    }
+}
---
+++ Model/Repositories/IEmployeRpository.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Atelier5.Model.Repositories
+{
+    public interface IEmployeRpository
+    {
+     
+       Task<IEnumerable<Employee>> GetEmployees();
+     
+       Task<Employee> GetEmployee(int employeeId);
+    
+       Task<Employee> AddEmployee(Employee employee);
+     
+       Task<Employee> UpdateEmployee(Employee employee);
+     
+      Task<Employee> DeleteEmployee(int employeeId);
+
+      Task<Employee> GetEmployeeByEmail(string email);
+        Task<IEnumerable<Employee>> Search(string name, Gender? gender);
+      
+
+
+
+}
+}
